612fbcc97ce29becadea340dd7a9845ff2f6c57f
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-02-08 17:34:21 -0800

    Add fMRI PCA

--- publication/FigX.PCA.R
+++ publication/FigX.PCA.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(cowplot)
+library(readxl)
 
-#### Data ####
+#### RNAseq data ####
 load("data_clean/P337_BAL_data.RData")
 load("data_clean/P337_BAL_module_data.RData")
 
@@ -29,7 +30,9 @@ PCA1 <- ggplot(PCA.dat, aes(PC1, PC2)) +
   theme_classic() +
   labs(x=PC1.label, y=PC2.label) +
   coord_fixed(ratio=1) +
-  theme(legend.position = "none")
+  scale_color_discrete(labels = c("Pre", "Post"),
+                       name = c("Allergen challenge")) +
+  theme(legend.position = "bottom")
 PCA1
 
 #### Module PCA ####
@@ -56,17 +59,66 @@ PCA2 <- ggplot(PCA.dat, aes(PC1, PC2)) +
   #Beautify
   theme_classic() +
   labs(x=PC1.label, y=PC2.label) +
-  coord_fixed(ratio=1) +
-  scale_color_discrete(labels = c("Pre", "Post"),
-                       name = c("Allergen challenge")) +
-  theme(legend.position = "top")
+  coord_fixed(ratio=1)  +
+  theme(legend.position = "none")
   
 #PCA2
 
-#### Save ####
+#### fMRI data ####
+#Time point 1 = visit 4
+neuro <- read_excel(sheet="T1",
+                    "data_raw/addtl.data/extraced.clusters.Matt.Altman_wbaseline_psychdata.xlsx") %>% 
+  #long format
+  pivot_longer(-idnum, names_to="neuro") %>% 
+  #add sample variable
+  mutate(sampID=paste("MA", idnum,"_V4",sep=""))
 
-plot <- plot_grid(PCA1,PCA2, ncol=1,
-          labels = c("(A) Genes", "(B) Modules"), hjust = 0)
+#Time point 2 = visit 5
+neuro <- read_excel(sheet="T2",
+                    "data_raw/addtl.data/extraced.clusters.Matt.Altman_wbaseline_psychdata.xlsx") %>% 
+  #long format
+  pivot_longer(-idnum, names_to="neuro") %>% 
+  #add sample variable
+  mutate(sampID=paste("MA", idnum, "_V5",sep=""))  %>% 
+  #Combine with other visit
+  full_join(neuro) %>% 
+  #wide format
+  pivot_wider(names_from = neuro) %>% 
+  select(-BDI, -LSI, -idnum) %>% 
+  column_to_rownames("sampID")
+
+#### fMRI PCA ####
+PCA <- neuro %>% 
+  prcomp()
+
+PC1.label <- paste("PC1 (", round(summary(PCA)$importance[2,1], digits=3)*100, "%)", sep="")
+PC2.label <-paste("PC2 (", round(summary(PCA)$importance[2,2], digits=3)*100, "%)", sep="")
+
+# Extract PC values
+PCA.dat <- as.data.frame(PCA$x) %>% 
+  rownames_to_column("sampID") %>%
+  # Select PCs
+  dplyr::select(sampID, PC1:PC3) %>% 
+  # Add  metadata
+  separate(sampID, into=c("donorID","visit"), sep="_", remove = FALSE)
+
+PCA3 <- ggplot(PCA.dat, aes(PC1, PC2)) +
+  geom_line(aes(group=donorID), color="grey") +
+  geom_point(aes(color=visit),
+             size=3) +
+  #Beautify
+  theme_classic() +
+  labs(x=PC1.label, y=PC2.label) +
+  coord_fixed(ratio=1)  +
+  theme(legend.position = "none")
+
+#PCA3
+
+#### Save ####
+plot.R <- plot_grid(PCA2,PCA3, ncol=1,
+                    labels = c("(B) Modules", "(C) fMRI"), hjust=0)
+plot <- plot_grid(PCA1,plot.R, nrow=1,
+          labels = c("(A) Genes", ""), hjust=0)
 plot
 
-ggsave("publication/FigX.PCA.png", height=7, width=4.5)
+ggsave("publication/FigX.PCA.png", height=6, width=8)
